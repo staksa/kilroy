3d0ba1f81278171a7eea6789b4669a7cb24bdc04
arekom <arek.komarnicki@gmail.com>
2013-04-27 01:49:15 +0100

    updated kilroy after meeting stacey

---
+++ public/js/disqus.js
@@ -0,0 +1,19 @@
+//disqus snippet
+var disqus_shortname = 'arekkomarnicki';
+
+(function () {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
+
+
+(function () {
+    var s = document.createElement('script');
+    s.async = true;
+    s.type = 'text/javascript';
+    s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
